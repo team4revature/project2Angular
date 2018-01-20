d26246c67887376fe845e486a2be0bec1b2e2c27
Sean Sung <seansosu@gmail.com>
2018-01-20 14:16:51 -0500

    updated signin page with componenet

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SigninComponent]
 })
 export class AppModule { }
