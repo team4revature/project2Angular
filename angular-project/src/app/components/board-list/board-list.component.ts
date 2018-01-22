020bae9e53fe87e03e191c7c5bcca297b6ab3d59
Patrick Kennedy <patrickjk94@gmail.com>
2018-01-22 16:37:33 -0500

    Fixed a bug.

--- angular-project/src/app/components/board-list/board-list.component.ts
+++ angular-project/src/app/components/board-list/board-list.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-//import { BoardListServiceService } from '../components/board-list-service.service';
-=======
 import { BoardListService } from '../../services/board-list-service.service';
->>>>>>> 137d63ddb6ebe5664aa22e01cf94bd81f7b34ae8
 import { BoardItemComponent } from '../board-item/board-item.component';
 
 @Component({
