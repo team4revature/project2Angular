5018a334ae8cba67139d3d3f769b4033a260184d
Sean Sung <seansosu@gmail.com>
2018-01-22 10:35:59 -0500

    separated navbar component

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,30 +1,45 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
 
+import { Router } from '@angular/router';
+
+// components
 import { AppComponent } from './app.component';
-import { StoryComponent } from './story/story.component';
-import { MatCardModule } from '@angular/material/card';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatCardModule } from '@angular/material/card'; 
 import { SigninComponent } from './components/signin/signin.component';
+import { StoryComponent } from './components/story/story.component';
+import { BurndownComponent } from './components/burndown/burndown.component';
 
-//services
+
+// services
 import { UserService } from './services/user.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent, StoryComponent, SigninComponent, NavbarComponent
+    AppComponent,
+    SigninComponent,
+    StoryComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    MatCardModule
+     BrowserModule,
+     FormsModule,
+     AppRoutingModule,
+     HttpClientModule,
+     MatCardModule
   ],
-  providers: [UserService],
-  bootstrap: [SigninComponent]
+  providers: [ UserService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+ }
