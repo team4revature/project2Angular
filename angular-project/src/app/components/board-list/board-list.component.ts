c12854ed3b777ba0398fcc87cf1d79b7f68a1173
Destinatus <destinatus@live.com>
2018-01-22 10:41:48 -0500

    Added board list components.

---
+++ angular-project/src/app/components/board-list/board-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { BoardListServiceService } from '../board-list-service.service';
+import { BoardItemComponent } from '../board-item/board-item.component';
+
+@Component({
+  selector: 'app-board-list',
+  templateUrl: './board-list.component.html',
+  styleUrls: ['./board-list.component.css']
+})
+export class BoardListComponent implements OnInit {
+
+  public  boards = [];
+  constructor(private _boardListService: BoardListServiceService) {}
+
+  ngOnInit() {
+    this.boards = this._boardListService.getBoardListByUser();
+    console.log(this.boards);
+  }
+
+}
